64b662e8cd73768696f51eeb9b30bbbde00de7f8
malay <malayjiyani1987@gmail.com>
2023-12-29 10:08:58 +0530

    Uploaded

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { createImageURL } from "../services/MovieServices";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const MovieItem = ({ movie }) => {
+  const [like, setLike] = useState(false);
+
   const { title, backdrop_path, poster_path } = movie;
   return (
     <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2">
@@ -14,6 +17,19 @@ const MovieItem = ({ movie }) => {
       <div className="absolute top-0 left-0 w-full h-[170px] bg-black/80 opacity-0 hover:opacity-100">
         <p className="whitespace-normal text-xs md:text-sm flex justify-center items-center h-full font-nsans-bold">
           {title}
+        </p>
+        <p>
+          {like ? (
+            <FaHeart
+              size={20}
+              className="absolute top-2 left-2 text-gray-300"
+            />
+          ) : (
+            <FaRegHeart
+              size={20}
+              className="absolute top-2 left-2 text-gray-300"
+            />
+          )}
         </p>
       </div>
     </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [rememberLogin, setRememberLogin] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
   return (
-    <div>
-      SignUp
-    </div>
-  )
-}
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="///"
+        />
+        <div className="bg-black/70 fixed top-0 left-0 w-full h-screen"></div>
+
+        <div className="fixed w-full px-4 py-24 z-20">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/80 rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-nsans-bold">Sign Up</h1>
+
+              <form
+                onSubmit={handleFormSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button className="bg-red-600 py-3 my-6 rounded font-nsans-bold">
+                  Sign Up
+                </button>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <p>
+                    <input
+                      type="checkbox"
+                      className="mr-2"
+                      checked={rememberLogin}
+                      onChange={(e) => setRememberLogin(!rememberLogin)}
+                    />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <div className="my-4">
+                  <span className="text-gray-600 mr-2 text-sm">
+                    Already subscribe to Netflix?
+                  </span>
+                  <Link to="/login">Sign In</Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
